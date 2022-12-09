35505140b0f3a9252c7314442119d198b1f16fa9
Noufal S <noufal@docme.cloud>
2022-12-09 17:39:21 +0530

    ok

--- application/admin/!model/actionlog.php
+++ application/admin/!model/actionlog.php
@@ -3,4 +3,4 @@
     |  ##CreativeSol Management Information System##   |
     |__________________________________________________|
 */
- class actionlog extends db_table { protected $_table = "\143\157\162\x65\x5f\141\x63\x74\x69\x6f\156\137\154\157\147"; protected $_pkey = "\141\x6c\x6f\x67\x5f\151\144"; public function add($npJXn) { return parent::insert($npJXn, "\154\x6f\x67"); } }
+ class actionlog extends db_table { protected $_table = "\x63\x6f\x72\x65\x5f\141\143\x74\151\157\x6e\x5f\154\x6f\x67"; protected $_pkey = "\141\x6c\x6f\147\137\x69\144"; public function add($npJXn) { return parent::insert($npJXn, "\154\x6f\x67"); } }
--- application/admin/!model/loginlog.php
+++ application/admin/!model/loginlog.php
@@ -3,4 +3,4 @@
     |  ##CreativeSol Management Information System##   |
     |__________________________________________________|
 */
- class loginlog extends db_table { protected $_table = "\x63\x6f\x72\x65\x5f\154\157\147\151\156\137\x6c\157\x67"; protected $_pkey = "\154\x6f\x67\x5f\x69\144"; public function add($npJXn) { return parent::insert($npJXn, "\154\x6f\147"); } }
+ class loginlog extends db_table { protected $_table = "\x63\157\162\145\137\x6c\157\147\151\x6e\137\x6c\157\x67"; protected $_pkey = "\x6c\157\x67\x5f\x69\x64"; public function add($npJXn) { return parent::insert($npJXn, "\154\157\147"); } }
